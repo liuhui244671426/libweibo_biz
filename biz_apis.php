5a13d0ddece1f92a3d3df1d0409a27e8c3ce1941
liuhui <liuhui244671426@126.com>
2016-04-18 14:46:58 +0800

    提交文件1

---
+++ biz_apis.php
@@ -0,0 +1,506 @@
+<?php
+
+/**
+ * 商业数据接口 demo
+ *
+ * 错误代码说明: http://open.weibo.com/wiki/Error_code
+ *
+ * author liuhui9<liuhui9@staff.sina.com.cn>
+ * @version 16/3/17
+ * @copyright copyright(2016) weibo.com all rights reserved
+ */
+
+require_once 'libweibo/saetv2.ex.class.php';
+
+
+class biz_apis
+{
+    const C_API_2_URL = 'https://c.api.weibo.com/2/';
+    const C_API_1_URL = 'https://c.api.weibo.com/';//订阅接口还未切换过来
+    //private $_since_id = false;//订阅-上次连接断开时的数据ID
+
+    public function __construct($appkey, $appsecret, $token)
+    {
+        $this->oauth = new SaeTOAuthV2($appkey, $appsecret, $token);
+        $this->oauth->host = self::C_API_2_URL;
+        $this->oauth->debug = true;
+    }
+
+    /*搜索最近数据（收费）*/
+    /**
+     * 搜索含某关键词的微博 。
+     *
+     * @pamram  string  q            搜索的关键字( 不能包含{、}、“ 等特殊字符)。必须进行URLencode。
+     * @pamram  string  ids            搜索指定批量用户的微博，传用户UID，多个用~分隔，最多不超过50个。
+     * @pamram  int     province    搜索的省份ID，参考省份城市编码表，参数q为空表示搜索这个省份的所有微博。
+     * @pamram  int     city        搜索的城市ID，参考省份城市编码表，参数q为空表示搜索这个城市的所有微博。
+     * @pamram  string  sort        排序方式，time：时间倒序、hot：热门度、fwnum：按转发数倒序、cmtnum：评论数倒序，默认为time。
+     * @pamram  int     starttime    搜索范围起始时间，取值为时间戳，单位为秒。
+     * @pamram  int     endtime        搜索范围结束时间，取值为时间戳，单位为秒。
+     * @pamram  int     hasori        是否包含原创，0：不包含原创、1：只包含原创，默认为空（全部）。
+     * @pamram  int     hasret        是否包含转发，0：不包含转发、1：只包含转发，默认为空（全部）。
+     * @pamram  int     hastext        是否包含纯文本，0：不包含纯文本、1：只包含纯文本，默认为空（全部）。
+     * @pamram  int     haspic        是否包含图片，0：不包含图片、1：只包含图片，默认为空（全部）。
+     * @pamram  int     hasvideo    是否包含视频，0：不包含视频、1：只包含视频，默认为空（全部）。
+     * @pamram  int     hasmusic    是否包含音乐，0：不包含音乐、1：只包含音乐，默认为空（全部）。
+     * @pamram  int     haslink        是否包含链接，0：不包含链接、1：只包含链接，默认为空（全部）。
+     * @pamram  int     hasat        是否包含@，0：不包含@、1：只包含@，默认为空（全部）。
+     * @pamram  int     hasv        是否为v用户发言，0：否、1：是，默认为空（全部）。
+     * @pamram  int     istag        是否严格为搜##内的话题，0：否、1：##内模糊匹配、2：##内精确匹配，默认为0。
+     * @pamram  int     onlynum        是否只返回总数，0：否、1：是，默认为0。
+     * @pamram  int     dup         是否排重（不显示相似数据），0：否、1：是，默认为1。
+     * @pamram  int     antispam    是否反垃圾（不显示低质量数据），0：否、1：是，默认为1。
+     * @pamram  int     page        页码，默认为1。
+     * @pamram  int     count        每页返回的数量，默认10，最大50。（默认返回10条）
+     * @pamram  int     base_app    是否只获取当前应用的数据。0为否（所有数据），1为是（仅当前应用），默认为0。
+     *
+     *
+     *
+     *
+     *
+     * wiki:http://open.weibo.com/wiki/C/2/search/statuses/limited
+     * */
+    public function search_statuses_limited($q, $ids = false, $province = false, $city = false, $sort = 'time', $starttime = false, $endtime = false,
+                                            $hasori = '', $hasret = '', $hastext = '', $haspic = '', $hasvideo = '', $hasmusic = '',
+                                            $haslink = '', $hasat = '', $hasv = '', $istag = 0, $onlynum = 0, $dup = 1, $antispam = 1,
+                                            $page = 1, $count = 10, $base_app = 0)
+    {
+        $params = array(
+            'q'         => $q,
+            'ids'       => $ids,
+            'province'  => $province,
+            'city'      => $city,
+            'sort'      => $sort,
+            'starttime' => $starttime,
+            'endtime'   => $endtime,
+            'hasori'    => $hasori,
+            'hasret'    => $hasret,
+            'hastext'   => $hastext,
+            'haspic'    => $haspic,
+            'hasvideo'  => $hasvideo,
+            'hasmusic'  => $hasmusic,
+            'haslink'   => $haslink,
+            'hasat'     => $hasat,
+            'hasv'      => $hasv,
+            'istag'     => $istag,
+            'onlynum'   => $onlynum,
+            'dup'       => $dup,
+            'antispam'  => $antispam,
+            'page'      => $page,
+            'count'     => $count,
+            'base_app'  => $base_app,
+        );
+        $params = $this->rid_false_for_array($params);
+        //print_r($params);
+        //die;
+
+        $ret = $this->oauth->get('search/statuses/limited', $params);
+        return $ret;
+    }
+
+    /*搜索最近数据（收费）*/
+
+
+    /*微博内容数据（收费）*/
+    /**
+     * 返回一条微博的全部转发微博列表。
+     *
+     * @param int $id 需要查询的微博ID。
+     * @param int $count 单页返回的记录条数，最大不超过200，默认为20。
+     * @param int $page 返回结果的页码，默认为1。
+     * @param int $filter_by_author 作者筛选类型，0：全部、1：我关注的人、2：陌生人，默认为0。
+     * @return mixed
+     *
+     * wiki:http://open.weibo.com/wiki/C/2/statuses/repost_timeline/all
+     * */
+    public function statuses_repost_timeline_all($id, $count = 20, $page = 1, $filter_by_author = 0)
+    {
+
+        $params = array(
+            'id'               => $id,
+            'count'            => $count,
+            'page'             => $page,
+            'filter_by_author' => $filter_by_author,
+        );
+
+        $ret = $this->oauth->get('statuses/repost_timeline/all', $params);
+        return $ret;
+    }
+
+    /**
+     * 批量获取用户个人微博列表。
+     * wiki:http://open.weibo.com/wiki/C/2/statuses/user_timeline_batch
+     * */
+    public function statuses_user_timeline_batch($uids, $count = 20, $page = 1, $flag = 0, $base_app = 0, $feature = 0)
+    {
+        $params = array(
+            'uids'     => $uids,
+            'count'    => $count,
+            'page'     => $page,
+            'flag'     => $flag,
+            'base_app' => $base_app,
+            'feature'  => $feature,
+        );
+        $params = $this->rid_false_for_array($params);
+        $ret = $this->oauth->get('statuses/user_timeline_batch', $params);
+        return $ret;
+    }
+
+    /**
+     * 获取@某人的微博。
+     * http://open.weibo.com/wiki/C/2/statuses/mentions/other
+     *
+     * */
+    public function statuses_mentions_other($uid, $since_id = 0, $max_id = 0, $count = 20, $page = 1, $filter_by_author = 0, $filter_by_source = 0, $filter_by_type = 0)
+    {
+        $params = array(
+            'uid'              => $uid,
+            'since_id'         => $since_id,
+            'max_id'           => $max_id,
+            'count'            => $count,
+            'page'             => $page,
+            'filter_by_author' => $filter_by_author,
+            'filter_by_source' => $filter_by_source,
+            'filter_by_type'   => $filter_by_type,
+        );
+
+        $ret = $this->oauth->get('statuses/mentions/other', $params);
+        return $ret;
+    }
+
+    /**
+     * 获取某个用户的位置动态。
+     *
+     * wiki:http://open.weibo.com/wiki/C/2/place/user_timeline/other
+     * */
+    public function place_user_timeline_other($uid, $since_id = 0, $max_id = 0, $count = 20, $page = 1, $base_app = 0)
+    {
+        $params = array(
+            'uid'      => $uid,
+            'since_id' => $since_id,
+            'max_id'   => $max_id,
+            'count'    => $count,
+            'page'     => $page,
+            'base_app' => $base_app,
+        );
+        $ret = $this->oauth->get('place/user_timeline/other', $params);
+        return $ret;
+    }
+
+    /**
+     *
+     * 返回一条微博的全部评论列表。
+     * wiki:http://open.weibo.com/wiki/C/2/comments/show/all
+     * */
+    public function comments_show_all($id, $count = 50, $page = 1, $filter_by_author = 0)
+    {
+        $params = array(
+            'id'               => $id,
+            'count'            => $count,
+            'page'             => $page,
+            'filter_by_author' => $filter_by_author,
+        );
+        $ret = $this->oauth->get('comments/show/all', $params);
+        return $ret;
+    }
+
+    /**
+     * 获取某个用户发出的评论列表
+     * wiki:http://open.weibo.com/wiki/C/2/comments/by_me/other
+     * */
+    public function comments_by_me_other($uid, $since_id = 0, $max_id = 0, $count = 50, $page = 1, $filter_by_type = 0)
+    {
+        $params = array(
+            'uid'            => $uid,
+            'since_id'       => $since_id,
+            'max_id'         => $max_id,
+            'count'          => $count,
+            'page'           => $page,
+            'filter_by_type' => $filter_by_type,
+        );
+        $ret = $this->oauth->get('comments/by_me/other', $params);
+        return $ret;
+    }
+
+    /**
+     * 获取某个用户收到的评论列表
+     * wiki:http://open.weibo.com/wiki/C/2/comments/to_me/other
+     *
+     * */
+    public function comments_to_me_other($uid, $since_id = 0, $max_id = 0, $count = 50, $page = 1, $filter_by_author = 0, $filter_by_source = 0)
+    {
+        $params = array(
+            'uid'              => $uid,
+            'since_id'         => $since_id,
+            'max_id'           => $max_id,
+            'count'            => $count,
+            'page'             => $page,
+            'filter_by_author' => $filter_by_author,
+            'filter_by_source' => $filter_by_source,
+        );
+        $ret = $this->oauth->get('comments/to_me/other', $params);
+        return $ret;
+    }
+
+    /**
+     * 获取某个用户发出和收到的评论列表
+     * wiki:http://open.weibo.com/wiki/C/2/comments/timeline/other
+     * */
+    public function comments_timeline_other($uid, $since_id = 0, $max_id = 0, $count = 50, $page = 1, $trim_user = 0)
+    {
+        $params = array(
+            'uid'       => $uid,
+            'since_id'  => $since_id,
+            'max_id'    => $max_id,
+            'count'     => $count,
+            'page'      => $page,
+            'trim_user' => $trim_user,
+        );
+        $ret = $this->oauth->get('comments/timeline/other', $params);
+        return $ret;
+    }
+
+    /**
+     * 获取@某人的评论
+     * wiki:http://open.weibo.com/wiki/C/2/comments/mentions/other
+     * */
+    public function comments_mentions_other($uid, $since_id = 0, $max_id = 0, $count = 50, $page = 1, $filter_by_author = 0, $filter_by_source = 0)
+    {
+        $params = array(
+            'uid'              => $uid,
+            'since_id'         => $since_id,
+            'max_id'           => $max_id,
+            'count'            => $count,
+            'page'             => $page,
+            'filter_by_author' => $filter_by_author,
+            'filter_by_source' => $filter_by_source,
+        );
+        $ret = $this->oauth->get('comments/mentions/other', $params);
+        return $ret;
+    }
+
+
+    /*微博内容数据（收费）*/
+
+
+    /*检索历史全量数据（收费）*/
+
+    /**
+     * 创建检索历史数据任务。
+     * wiki:http://open.weibo.com/wiki/C/2/search/statuses/historical/create
+     * */
+    public function search_statuses_historical_create($q, $ids = false, $province = false, $city = false, $starttime = false, $endtime = false, $type = false, $hasv = false, $onlynum = 100)
+    {
+        $params = array(
+            'q'         => $q,
+            'ids'       => $ids,
+            'province'  => $province,
+            'city'      => $city,
+            'starttime' => $starttime,
+            'endtime'   => $endtime,
+            'type'      => $type,
+            'hasv'      => $hasv,
+            'onlynum'   => $onlynum,
+        );
+        $params = $this->rid_false_for_array($params);
+        $ret = $this->oauth->post('search/statuses/historical/create', $params);
+        return $ret;
+    }
+
+    /**
+     * 查看检索历史数据任务的执行状态。
+     * wiki:http://open.weibo.com/wiki/C/2/search/statuses/historical/check
+     *
+     *
+     * @param int $id 创建任务者的ID, 类似APPKEY
+     * @param string $secret_key 创建后返回的字段 secret_key
+     * @return array
+     * status 任务执行状态，true:完成，false:执行中
+     * count 任务结果条数，当status为false时，该字段返回0
+     * */
+    public function search_statuses_historical_check($id, $task_id, $timestamp, $secret_key)
+    {
+        //认证签名。
+        //加密方式为signature = md5(id + secret_key + timestamp)。id为任务创建者ID，id,secret_key平台方通过task_id得到。
+        $signature = md5($id . $secret_key . $timestamp);
+        $params = array(
+            'task_id'   => $task_id,
+            'timestamp' => $timestamp,
+            'signature' => $signature,
+        );
+        $ret = $this->oauth->get('search/statuses/historical/check', $params);
+        return $ret;
+    }
+    
+    /**
+     * 下载任务执行后的结果数据
+     * wiki:http://open.weibo.com/wiki/C/2/search/statuses/historical/download
+     * */
+    public function search_statuses_historical_download($id, $task_id, $timestamp, $secret_key)
+    {
+        //认证签名。
+        //加密方式为signature = md5(id + secret_key + timestamp)。id为任务创建者ID，id,secret_key平台方通过task_id得到。
+        $signature = md5($id . $secret_key . $timestamp);
+        $params = array(
+            'task_id'   => $task_id,
+            'timestamp' => $timestamp,
+            'signature' => $signature,
+        );
+        $ret = $this->oauth->get('search/statuses/historical/download', $params);
+        return $ret;
+
+    }
+    /*检索历史全量数据（收费）*/
+
+    /**
+     *
+     * wiki:http://open.weibo.com/wiki/C/2/statuses/upload/biz
+     * */
+    public function statuses_upload_biz($status, $pic, $visible = 0, $list_id = false, $lat = 0.0, $long = 0.0,
+                                        $annotations = false, $rip = false)
+    {
+        $params = array(
+            'status'      => $status,
+            'visible'     => $visible,
+            'list_id'     => $list_id,
+            'pic'         => '@' . $pic,
+            'lat'         => $lat,
+            'long'        => $long,
+            'annotations' => $annotations,
+            'rip'         => $rip,
+        );
+
+        $params = $this->rid_false_for_array($params);
+        $ret = $this->oauth->post('statuses/upload/biz', $params, true);
+        return $ret;
+    }
+    
+    /**
+     * 订阅关键词、用户。
+     *
+     * wiki:http://open.weibo.com/wiki/C/2/subscribe/update_subscribe
+     * */
+    public function subscribe_update_subscribe($subid, $source, $access_token = false, $add_keywords = false, $del_keywords = false, $add_uids = false, $del_uids = false, $_version=1)
+    {
+        $params = array(
+            'source' => $source,
+            'access_token' => $access_token,
+            'subid' =>  $subid,
+            'add_keywords'  => $add_keywords,
+            'del_keywords'  => $del_keywords,
+            'add_uids' =>  $add_uids,
+            'del_uids' =>  $del_uids,
+        );
+        $this->oauth->host = ($_version == 1)
+            ?   self::C_API_1_URL  :   self::C_API_2_URL;
+        $params = $this->rid_false_for_array($params);
+        $ret = $this->oauth->post('subscribe/update_subscribe', $params, true);
+        return $ret;
+    }
+
+    /**
+     * 进行连接，接收微博数据。
+     *
+     * @param int $_version 暂时只支持c.api.weibo.com
+     * wiki:http://open.weibo.com/wiki/C/2/datapush/status
+     * */
+    public function datapush_status($subid, $source = false, $since_id = false, $_version=1)
+    {
+        $params = array(
+            'subid' =>  $subid,
+            'source' => $source,
+            'since_id'  => $since_id,
+        );
+        $this->oauth->host = ($_version == 1)
+            ?   'http://c.api.weibo.com/'  :   self::C_API_2_URL;
+        $params = $this->rid_false_for_array($params);
+        $url = $this->oauth->host.'datapush/status';
+
+        $ret = $this->push_method($url, $params);
+
+        //debug code
+//        require_once 'libs/httpWrap.php';
+//        $obj = new HttpWrap();
+//        $params = http_build_query($params);
+//        $url .= '?'.$params;
+//        $ret = $obj->init($url);
+
+
+        //var_dump($ret);
+        //return $ret;
+    }
+    public function push_method($url, $params){
+        $parse_url = parse_url($url);
+        $host = $parse_url['host'];
+        $path = $parse_url['path'];
+        $query = http_build_query($params);
+
+        $fp = fsockopen($host, 80, $errno, $errstr, 30);
+        stream_set_timeout($fp, 30);
+        $ret = '';
+        if (!$fp) {
+            echo "$errstr ($errno)<br />\n";
+        } else {
+            $requestHeader = "GET {$path}?{$query} HTTP/1.1\r\n";
+            $requestHeader .= "Host: {$host}\r\n";
+            $requestHeader .= "Connection: Close\r\n\r\n";
+            //fwrite($fp, $requestHeader);
+            fputs($fp, $requestHeader);
+            stream_set_blocking($fp, true);//阻塞
+
+            $responseHeader = '';
+            $responseContent = '';
+            $chunk_size = (integer)hexdec(fgetss( $fp, 4096 ) );
+            $i = 1;//小切片
+            $ii = 1;//大切片
+            while (!feof($fp) && $chunk_size > 0) {
+                if($i <= 10){
+                    $responseContent .= fgetss($fp, 4096);
+                    $i++;
+                } else {
+                    $i = 1;
+                    //var_dump($responseContent);
+                    $content = $this->unchunkHttp11Json($responseContent);
+
+                    var_dump($content);
+                    $content = '';
+                    $ii++;
+                    if($ii == 5)die;
+                }
+
+            }
+            fclose($fp);
+        }
+        return $ret;
+    }
+
+
+    function unchunkHttp11Json($data) {
+        $outData = array();
+        $content = explode("\r\n", $data);
+
+        foreach ($content as $k => $v) {
+            $chunk = json_decode($v, true);
+            //is json string
+            if(json_last_error() == JSON_ERROR_NONE){
+                if(!empty($chunk)){
+                    $outData[] = $chunk;
+                }
+            }
+
+        }
+        return $outData;
+    }
+    /**
+     * 将数组中 false 字段删除
+     * */
+    private function rid_false_for_array($arr)
+    {
+        foreach ($arr as $k => $v) {
+            if ($v === false || $v === null) {
+                unset($arr[ $k ]);
+            }
+        }
+        return $arr;
+    }
+}
---
+++ test_biz_apis.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ *
+ *
+ * author liuhui9<liuhui9@staff.sina.com.cn>
+ * @version 16/3/22
+ * @copyright copyright(2016) weibo.com all rights reserved
+ */
+require_once 'biz_apis.php';
+ini_set('date.timezone','Asia/Shanghai');
+set_time_limit(0);
+
+define('APPKEY', 3047579336);
+define('APPSECRET', 'd7d8e3ff23d32a1fad3df1f5c642e4b3');
+define('TOKEN', '2.005Xmn4G7b1P1D53300889490hGUeY'); //$token = '2.00DSk7ADCSnslC3c5a275207JZ_vEE';//文奇
+define('SUBID', '10592');//订阅id
+
+$object = new biz_apis(APPKEY, APPSECRET, TOKEN);
+$keyword = urlencode('PHPER');
+$weiboID = '3953931363797697';
+$uid = 1748248662;
+$stime = strtotime('2012-03-21 00:00:00').'000';
+$etime = strtotime('2012-03-22 00:00:00').'000';
+//任务参数
+$taskID = 12421179;
+$secret_key = '9efa06a6f23d0ddb7217';
+//订阅服务
+$sub_keywords = '阿里巴巴,楼市,股市';
+$sub_uids = '';
+
+//ok
+//$ret = $object->search_statuses_limited($keyword);
+//ok
+//$ret = $object->statuses_repost_timeline_all($weiboID);
+//还有点问题,无返回值
+//无返回值的问题已经查明, 是命令行的 php curl 出现 SSLRead() return error -9806 的错误,如何修复还待确认
+//$ret = $object->statuses_upload_biz('我是测试1234', 'f.jpeg');
+//ok
+//$ret = $object->statuses_mentions_other($uid);
+//ok
+//$ret = $object->statuses_user_timeline_batch($uid);
+//ok
+//$ret = $object->place_user_timeline_other($uid);
+//ok
+//$ret = $object->comments_show_all($weiboID);
+//ok
+//$ret = $object->comments_by_me_other($uid);
+//ok
+//$ret = $object->comments_to_me_other($uid);
+//ok
+//$ret = $object->comments_timeline_other($uid);
+//ok
+//$ret = $object->comments_mentions_other($uid);
+//ok
+//$ret = $object->search_statuses_historical_create($keyword, false,false,false,$stime,$etime);
+//ok
+//$ret = $object->search_statuses_historical_check(APPKEY, $taskID, time(), $secret_key);
+//$ret = $object->search_statuses_historical_download(APPKEY, $taskID, time(), $secret_key);
+
+//$ret = $object->subscribe_update_subscribe(SUBID, APPKEY, false, $sub_keywords);
+$ret = $object->datapush_status(SUBID, APPKEY);
+
+
+
+
+
+//var_dump($ret);
+
+
+die;
